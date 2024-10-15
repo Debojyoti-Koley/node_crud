e1f55a8688bda540e7ed56ea62043c6f0b05a871
Debojyoti <71626714+Debojyoti-Koley@users.noreply.github.com>
2024-10-15 22:04:50 +0530

    authentication v1

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 // define person schema
 const personSchema = new mongoose.Schema({
@@ -12,9 +13,51 @@ const personSchema = new mongoose.Schema({
     },
     city:{
         type: String
+    },
+    work:{
+        type: String,
+        enum: ['chef','waiter','manager'],
+        require: true   
+    },
+    username:{
+        require: true,
+        type: String
+    },
+    password:{
+        required: true,
+        type: String
     }
 })
 
+personSchema.pre('save', async function(next){
+    const person = this;
+
+    //Hash the password only if it is modified or new
+    if(!person.isModified('password'))
+        return next()
+    try{
+        //hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        //override the pain password with hash
+        person.password = hashedPassword;
+        next();
+    }
+    catch(err){
+        return next(err);
+    }
+})
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        const isMatch = await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+    }
+    catch(err){
+        throw err;
+    }
+}
 //create person model
 const Person = mongoose.model('Person', personSchema);
-module.exports = Person
+module.exports = Person;
--- server.js
+++ server.js
@@ -5,8 +5,41 @@ const Person = require('./models/person')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
 
-app.get('/', function (req, res) {
+// Middleware function
+const logRequest = (req,res,next) =>{
+  console.log(`[${new Date().toLocaleString}] Request made to ${req.originalUrl}`);
+  next()
+} 
+
+app.use(logRequest)
+
+app.use(passport.initialize())
+
+passport.use(new localStrategy(async (entered_username, entered_password, done)=>{
+  // authentication logic
+  try{
+    const user =  await Person.findOne({username: entered_username})
+    if(!user){
+      return done(null, false, {message: 'incorrect username'})
+    }
+    const isPasswordMatch = await user.comparePassword(entered_password);
+    if(isPasswordMatch){
+      return done(null, user)
+    }
+    else{
+      return done(null, false, {message: "incorrect password"})
+    }
+  }catch(err){
+    return done(err);
+  }
+}))
+
+const localAuthMiddleware = passport.authenticate('local',{session:false})
+
+app.get('/',localAuthMiddleware, function (req, res) {
   res.send('Hello World')
 })
 
